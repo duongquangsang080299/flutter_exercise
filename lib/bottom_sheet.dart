c9f4cf9fc8e6e99615e432601f2e5a57e6c8098b
duongquangsang080299 <53465403+duongquangsang080299@users.noreply.github.com>
2023-07-26 14:53:09 +0700

    run example code bottom sheet

---
+++ lib/bottom_sheet.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('BottomSheet Example'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              showBottomSheet(context);
+            },
+            child: const Text('Show Bottom Sheet'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void showBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) {
+      return SizedBox(
+        height: 200,
+        child: Column(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.camera),
+              title: const Text('Take a Photo'),
+              onTap: () {
+                Navigator.pop(context); // Close the bottom sheet
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.photo_library),
+              title: const Text('Choose from Gallery'),
+              onTap: () {
+                Navigator.pop(context); // Close the bottom sheet
+              },
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
