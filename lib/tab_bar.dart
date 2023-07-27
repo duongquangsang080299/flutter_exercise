3ca33d790e8be0bdbb68b9fd38ff3888287852dd
duongquangsang080299 <53465403+duongquangsang080299@users.noreply.github.com>
2023-07-27 14:22:34 +0700

    run example code Tab Bar

---
+++ lib/tab_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3, // Number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('TabBar Example'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Home'),
+              Tab(icon: Icon(Icons.search), text: 'Search'),
+              Tab(icon: Icon(Icons.person), text: 'Profile'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text('Home Page')),
+            Center(child: Text('Search Page')),
+            Center(child: Text('Profile Page')),
+          ],
+        ),
+      ),
+    );
+  }
+}
