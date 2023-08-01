a214def81bcac2310a3812945bdf294e2c053fee
duongquangsang080299 <53465403+duongquangsang080299@users.noreply.github.com>
2023-08-01 14:25:50 +0700

    run example code scaffold reading

---
+++ lib/scaffold.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My App'),
+      ),
+      body: const Center(
+        child: Text('Welcome to my app!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your action here
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
