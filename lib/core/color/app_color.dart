2ab333dff50bc726629e653b2325217e5bc40f3e
duongquangsang080299 <53465403+duongquangsang080299@users.noreply.github.com>
2023-08-23 11:48:24 +0700

    " Update Theme about Color,Font Wheight,Text Style

---
+++ lib/core/color/app_color.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+/// Abstract class for defining app color
+abstract class AppColor {
+  ///Primary color for app
+  static const primaryColor = Color(0xFF04764E);
+
+  /// Background color for app
+  static const backgroundColor = Colors.white;
+
+  /// Surface color
+  static const surfaceColor = Color(0xFFF7F7F7);
+
+  /// Error color for indicating errors
+  static const errorColor = Colors.red;
+
+  /// Primary text color
+  static const textPrimary = Colors.black;
+
+  /// Secondary text color
+  static const textSecondary = Color(0xFF595959);
+
+  /// Text field color
+  static const textField = Color(0xFF8A8A8A);
+
+  /// Text color for cards
+  static const textCard = Color(0xFF6CAE97);
+
+  /// Neon silver color for boder buttton and text in button
+  static const neonSilverColor = Color(0xFFCBCBCB);
+
+  /// Outline Color for images
+  static const outLineColor = Color(0xFF00AD70);
+
+  /// Jet black color
+  static const jetBlackColor = Color(0xFF373737);
+
+  ///White smoke color
+  static const whiteSmokeColor = Color(0xFFF3F3F3);
+
+  /// Flash White Color
+  static const flashWhiteColor = Color(0xFFEFEFEF);
+
+  /// TextTypography color
+  static const textTypography = Color.fromRGBO(0, 0, 0, 0.50);
+}
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,42 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:soccer_club_app/core/color/colors.dart';
 
-import 'package:soccer_club_app/core/typography/text_style.dart';
+import 'package:soccer_club_app/core/color/app_color.dart';
+
+import 'package:soccer_club_app/core/typography/app_text_style.dart';
 
 class AppTheme {
+  /// Constructor for the AppTheme class
   const AppTheme();
 
   ThemeData get themeData {
     return ThemeData(
-      // brightness: Brightness.light,
-      primaryColor: AppColor.primary,
-      hintColor: AppColor.secondary,
+      /// brightness: Brightness.light,
+      /// Set the primary color
+      primary: AppColor.primaryColor,
+
+      /// Set the background color
+      background: AppColor.backgroundColor,
+
+      /// Call the _colorScheme getter
       colorScheme: _colorScheme,
-      scaffoldBackgroundColor: AppColor.background,
+
+      /// Set scaffold background color
+      scaffoldBackgroundColor: AppColor.backgroundColor,
+
+      /// Set the text theme
       textTheme: _textTheme,
     );
   }
 
   ColorScheme get _colorScheme {
     return const ColorScheme.light(
-      primary: AppColor.primary,
-      secondary: AppColor.secondary,
-      // onPrimary: AppColor.textOnPrimary,
-      // onSecondary: AppColor.textOnSecondary,
-      // error: AppColor.error,
-      // onError: Colors.white,
-      // background: AppColor.background,
-      // onBackground: AppColor.textPrimary,
-      // surface: AppColor.surface,
-      // onSurface: AppColor.textPrimary,
+      primary: AppColor.primaryColor,
+      background: AppColor.backgroundColor,
     );
   }
 
   static final TextTheme _textTheme = TextTheme(
+    /// Assign the defined text style
     displayLarge: AppTextStyles.titleLarge,
     displayMedium: AppTextStyles.titleMedium,
-    bodyLarge: AppTextStyles.description,
+    bodyLarge: AppTextStyles.extralBold,
     bodyMedium: AppTextStyles.bodyBold,
     titleMedium: AppTextStyles.subtitle,
     bodySmall: AppTextStyles.caption,
@@ -44,14 +47,14 @@ class AppTheme {
 }
 
 class AppDarkTheme extends AppTheme {
+  /// Constructor for the AppDarkTheme class
   const AppDarkTheme();
 
   @override
   ColorScheme get _colorScheme {
     return const ColorScheme.dark().copyWith(
-      primary: AppColor.primary,
-      secondary: AppColor.secondary,
-      background: AppColor.background,
+      primary: AppColor.primaryColor,
+      background: AppColor.backgroundColor,
     );
   }
 }
