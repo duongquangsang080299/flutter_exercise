2bcd50e542a0406f6389961db5ba0f3f4809e2eb
duongquangsang080299 <53465403+duongquangsang080299@users.noreply.github.com>
2023-08-25 08:20:13 +0700

    Update Project Soccer App

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:soccer_club_app/core/theme/app_theme.dart';
+import 'package:soccer_club_app/l10n/l10n.dart';
+import 'package:soccer_club_app/routes.dart';
+
+void main() => runApp(
+      DevicePreview(
+        builder: (context) => const App(),
+      ),
+    );
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: const AppTheme().themeData,
+      darkTheme: const AppDarkTheme().themeData,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('es'),
+      ],
+      routeInformationProvider: appRouter.routeInformationProvider,
+      routeInformationParser: appRouter.routeInformationParser,
+      routerDelegate: appRouter.routerDelegate,
+    );
+  }
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:soccer_club_app/bootstrap.dart';
+import 'package:soccer_club_app/main.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
