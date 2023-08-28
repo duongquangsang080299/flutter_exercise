ff4e602f82541af6e2392b37f0398e2e162794cf
Repo <duongquangsang080299@gmail.com>
2023-08-28 16:01:37 +0700

    Implemnt Scaffold Widget

---
+++ lib/widgets/scaffold.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+// Define class for building a custom scaffold widget
+class SCScaffold extends StatelessWidget {
+  // Constructor for the widget
+  const SCScaffold({
+    required this.body,
+    super.key,
+    this.appBar,
+    this.backgroundColor,
+    this.bottomNavigationBar,
+    this.resizeToAvoidBottomInset,
+  });
+  final PreferredSizeWidget? appBar;
+  final Widget body;
+  final Color? backgroundColor;
+  final Widget? bottomNavigationBar;
+
+  final bool? resizeToAvoidBottomInset;
+
+  // Method for building the widget
+  @override
+  Widget build(BuildContext context) {
+    // Return a Scaffold widget with the specified fields
+    return Scaffold(
+      resizeToAvoidBottomInset: resizeToAvoidBottomInset,
+      backgroundColor: backgroundColor,
+      appBar: appBar,
+      body: body,
+      bottomNavigationBar: bottomNavigationBar,
+    );
+  }
+}
