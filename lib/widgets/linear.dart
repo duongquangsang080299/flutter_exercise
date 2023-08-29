1046b0c10068ab3a9d761ab50ffd5f9f06a9f35a
duongquangsang080299 <duongquangsang080299@gmail.com>
2023-08-29 09:40:57 +0700

    Update Linear Percent Indicator Widget

--- lib/widgets/linear.dart
+++ lib/widgets/linear.dart
@@ -4,28 +4,30 @@ import 'package:percent_indicator/linear_percent_indicator.dart';
 class SCLinearPercentIndicator extends StatelessWidget {
   const SCLinearPercentIndicator({
     required this.percent,
-    this.color,
+    this.progressColor = Colors.blue,
+    this.backgroundColor = Colors.grey,
     this.width = 93,
-    this.height = 5,
+    this.lineHeight = 15,
     super.key,
   });
   // The percentage value to display in the indicator
   final double percent;
   // The color of the progress indicator
-  final Color? color;
+  final Color backgroundColor;
   // The width of the indicator
   final double width;
   // The height of the indicator
-  final double height;
+  final double lineHeight;
+  final Color progressColor;
 
   @override
   Widget build(BuildContext context) {
     return LinearPercentIndicator(
       width: width,
-      lineHeight: height,
+      lineHeight: lineHeight,
       percent: percent,
-      backgroundColor: Colors.grey,
-      progressColor: Colors.blue,
+      backgroundColor: backgroundColor,
+      progressColor: progressColor,
     );
   }
 }
