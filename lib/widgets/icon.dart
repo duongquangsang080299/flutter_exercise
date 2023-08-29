a3f60c92f0dbc49a16ba0bbeec81db73244746f1
Repo <duongquangsang080299@gmail.com>
2023-08-29 09:06:26 +0700

    Update Icon Widget

--- lib/widgets/icon.dart
+++ lib/widgets/icon.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class SCIcon extends StatelessWidget {
-  const SCIcon({
+class SCBackIcon extends StatelessWidget {
+  /// Constructor for the SCBackIcon widget
+  const SCBackIcon({
     required this.iconData,
     super.key,
     this.color,
     this.width = 24,
     this.height = 24,
+    this.padding,
   });
 
+  /// Properties for the SCBackIcon widget
   final IconData iconData;
   final Color? color;
   final double? width;
   final double? height;
+  final EdgeInsetsGeometry? padding;
 
   @override
   Widget build(BuildContext context) {
-    return Icon(
-      iconData,
-      color: color,
+    // Build method for the widget
+    return SizedBox(
+      height: height,
+      width: width,
+      child: IconButton(
+        // Apply padding or use zero padding if not provided
+        padding: padding ?? EdgeInsets.zero,
+        icon: const Icon(
+          Icons.arrow_back,
+        ),
+        onPressed: () => context.pop(),
+      ),
     );
   }
 }
