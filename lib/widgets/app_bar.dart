f1fc021f96afd104772f8b04babdfb6a772b0f86
duongquangsang080299 <duongquangsang080299@gmail.com>
2023-08-30 15:40:10 +0700

    Fix : Update App Bar Widgets

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,52 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+/// Define AppBar
 class SCAppBar extends StatelessWidget {
+  /// Constructor for the SCAppBar widget
   const SCAppBar({
     required this.title,
-    required this.subtitle,
-    required this.notificationIcon,
-    required this.backgroundColor,
-    required this.drawerIcon,
-    required this.onNotificationPressed,
-    required this.onDrawerPressed,
+    this.onNotificationPressed,
+    this.onDrawerPressed,
+    this.trailing,
+    this.leading,
+    this.subtitle,
+    this.actions,
+    this.onPressed,
     super.key,
   });
-  final String title;
-  final String subtitle;
-  final IconData notificationIcon;
-  final Color? backgroundColor;
-  final IconData drawerIcon;
-  final VoidCallback onNotificationPressed;
-  final VoidCallback onDrawerPressed;
 
-  @override
+  /// Factory constructor for creating a home app bar
+  factory SCAppBar.home({
+    required String title,
+    String? subtitle,
+    Widget? leading,
+    VoidCallback? onNotificationPressed,
+    VoidCallback? onDrawerPressed,
+  }) {
+    return SCAppBar(
+      title: title,
+      leading: leading ?? const SizedBox.shrink(),
+      subtitle: subtitle ?? '',
+      onDrawerPressed: onDrawerPressed,
+      onNotificationPressed: onNotificationPressed,
+    );
+  }
+
+  /// Factory constructor for creating a non-home app bar
+  factory SCAppBar.secondHome({
+    required String title,
+    Icon? trailing,
+    VoidCallback? onPressed,
+  }) {
+    return SCAppBar(
+      title: title,
+      trailing: trailing,
+      onPressed: onPressed,
+    );
+  }
+
+  final String title;
+  final VoidCallback? onNotificationPressed;
+  final VoidCallback? onDrawerPressed;
+  final VoidCallback? onPressed;
+  final Icon? trailing;
+  final Widget? leading;
+  final String? subtitle;
+  final List<Widget>? actions;
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(fontSize: 14),
-          ),
-          Text(
-            subtitle,
-            style: const TextStyle(fontSize: 24),
-          ),
-        ],
-      ),
-      actions: [
-        IconButton(
-          icon: Icon(notificationIcon),
-          onPressed: onNotificationPressed,
+      // Center title only if subtitle is not empty
+      centerTitle: (subtitle?.isEmpty ?? false) && false,
+      // Define the border shape of the app bar
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(30),
         ),
-      ],
-      backgroundColor: backgroundColor,
-      leading: IconButton(
-        icon: Icon(drawerIcon),
-        onPressed: onDrawerPressed,
       ),
+
+      /// Adjust title spacing based on presence of leading widget
+      titleSpacing: leading == null ? 0 : NavigationToolbar.kMiddleSpacing,
+
+      /// Set leading width based on presence of leading widget
+      leadingWidth: leading == null ? 0 : 56,
+
+      /// Define leading widget
+      leading: leading != null
+          ? IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => context.pop(),
+            )
+          : const SizedBox.shrink(),
+      // Define title widget
+      title: (subtitle?.isEmpty ?? false)
+          ? Text(title)
+          : Column(
+              children: [
+                Text(title),
+                Text(subtitle ?? ''),
+              ],
+            ),
+      // Define actions
+      actions: [
+        if (subtitle?.isNotEmpty ?? false) ...[
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: onNotificationPressed,
+          ),
+          const SizedBox(width: 10),
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: onDrawerPressed,
+          ),
+        ] else
+          IconButton(
+            icon: trailing ?? const Icon(Icons.message),
+            onPressed: onPressed,
+          ),
+      ],
     );
   }
 }
