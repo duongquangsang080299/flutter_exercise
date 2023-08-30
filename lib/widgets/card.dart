f020a554b4852b2ee5809420af1089e107ab3bf9
duongquangsang080299 <duongquangsang080299@gmail.com>
2023-08-30 19:54:37 +0700

    Fix : Update Card Widget

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -2,56 +2,69 @@ import 'package:flutter/material.dart';
 
 class SCCard extends StatelessWidget {
   const SCCard({
-    required this.title,
-    required this.subtitle,
-    required this.backgroundColor,
+    this.title,
+    this.subtitle,
+    this.backgroundColor,
     super.key,
+    this.image,
+    this.widthImage,
+    this.heightImage,
   });
 
-  factory SCCard.matchCard({
+  const factory SCCard.matchCard({
     required String title,
     required String subtitle,
-  }) {
-    return SCCard(
-      title: title,
-      subtitle: subtitle,
-      backgroundColor: Colors.grey,
-    );
-  }
+    Color? backgroundColor,
+    Key? key,
+  }) = _MatchCard;
 
-  factory SCCard.imageCard({
+  const factory SCCard.imageCard({
+    required ImageProvider<Object> image,
+    double? widthImage,
+    double? heightImage,
+    Key? key,
+  }) = _ImageCard;
+  const factory SCCard.boxCard({
     required String title,
     required String subtitle,
     required Color backgroundColor,
-  }) {
-    return SCCard(
-      title: title,
-      subtitle: subtitle,
-      backgroundColor: backgroundColor,
-    );
-  }
+    Key? key,
+  }) = _BoxCard;
 
-  final String title;
-  final String subtitle;
-
-  final Color backgroundColor;
+  final String? title;
+  final String? subtitle;
+  final ImageProvider<Object>? image;
+  final Color? backgroundColor;
+  final double? widthImage;
+  final double? heightImage;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: backgroundColor,
-      child: Padding(
-        padding: const EdgeInsets.all(16),
+    return const Card();
+  }
+}
+
+class _BoxCard extends SCCard {
+  const _BoxCard({
+    super.title,
+    super.subtitle,
+    super.backgroundColor,
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              title,
+              title!,
               style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
             Text(
-              subtitle,
+              subtitle!,
               style: const TextStyle(fontSize: 16),
             ),
           ],
@@ -61,60 +74,124 @@ class SCCard extends StatelessWidget {
   }
 }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Card(
-//       color: backgroundColor,
-//       elevation: 4,
-//       child: Column(
-//         children: [
-//           Container(
-//             width: double.infinity,
-//             height: 150,
-//             decoration: BoxDecoration(
-//               color: backgroundColor,
-//               borderRadius: const BorderRadius.only(
-//                 topLeft: Radius.circular(20),
-//                 topRight: Radius.circular(20),
-//               ),
-//             ),
-//             child: Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//               children: [
-//                 Image.asset(''), // Use the provided image path here
-//                 IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
-//               ],
-//             ),
-//           ),
-//           Container(
-//             width: double.infinity,
-//             height: 100,
-//             decoration: const BoxDecoration(
-//               color: Color.fromARGB(255, 63, 61, 61),
-//               borderRadius: BorderRadius.only(
-//                 bottomLeft: Radius.circular(20),
-//                 bottomRight: Radius.circular(20),
-//               ),
-//             ),
-//             child: const Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               children: [
-//                 Icon(Icons.email, color: Colors.amber),
-//                 Column(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     Text(''),
-//                     Text(''),
-//                   ],
-//                 ),
-//                 Icon(Icons.chrome_reader_mode),
-//                 SizedBox(width: 10),
-//                 Text('data'),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+class _ImageCard extends SCCard {
+  const _ImageCard({
+    required super.image,
+    super.key,
+    super.heightImage,
+    super.widthImage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 121,
+      height: 123,
+      child: Card(
+        child: Image(
+          image: image!,
+        ),
+      ),
+    );
+  }
+}
+
+class _MatchCard extends SCCard {
+  const _MatchCard({
+    required super.title,
+    required super.subtitle,
+    super.key,
+    super.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 142,
+      width: MediaQuery.of(context).size.width,
+      child: Card(
+        color: Colors.white,
+        elevation: 0,
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Color(0xFFF3F3F3),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(''), 
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.more_vert_outlined,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0, 5),
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 21, right: 24),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.email, color: Colors.amber),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 13,
+                            ),
+                            child: Column(
+                              children: [
+                                Text('123'),
+                                Text('123'),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.chrome_reader_mode),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('Champ')
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
