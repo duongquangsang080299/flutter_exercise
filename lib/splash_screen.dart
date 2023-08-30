fe26bcf9332b8fbefb538bdb6f2047e99569d0e0
duongquangsang080299 <duongquangsang080299@gmail.com>
2023-08-30 23:13:05 +0700

    Implement Splash Screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,38 +1,28 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart'; // Import GoRouter if you're using it
+import 'package:go_router/go_router.dart'; 
 import 'package:soccer_club_app/core/color/app_color.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-
+  Widget build(BuildContext context) {
     // Simulate a delay for demonstration purposes
     Timer(const Duration(seconds: 5), () {
       // Navigate to the next screen using GoRouter
       GoRouter.of(context).go('/onBoarding');
     });
-  }
 
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColor.primaryColor, // Use your primary color
+      backgroundColor: AppColor.primaryColor, 
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // Logo image
             Image.asset(
-              'assets/images/logo.png', // Replace with your image path
+              'assets/images/logo.png', 
               width: 123,
               height: 146,
             ),
