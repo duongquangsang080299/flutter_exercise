138ff10ada5f04d7e80061ef23436f3b964496b1
duongquangsang080299 <duongquangsang080299@gmail.com>
2023-08-30 11:35:28 +0700

    Fix: Update Input Widget

--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -1,40 +1,87 @@
 import 'package:flutter/material.dart';
 
-/// Define TextFormField widget with common properties
-class SCTextFormField extends StatelessWidget {
-  // Constructor for SCTextFormField
-  const SCTextFormField({
-    required this.controller,
+class SCInput extends StatelessWidget {
+  const SCInput({
     required this.labelText,
-    required this.hintText,
-    required this.validator,
     super.key,
-    this.keyboardType = TextInputType.text,
+    this.hintText,
+    this.focusNode,
+    this.errorMessage,
+    this.isPassword = false,
+    this.suffixIcon,
+    this.controller,
+    this.validator,
+    this.keyboardType,
     this.obscureText = false,
   });
 
+  /// Factory constructor for password input
+  factory SCInput.password({
+    required String labelText,
+    Key? key,
+    FocusNode? focusNode,
+    String? hintText,
+    Widget? suffixIcon,
+  }) {
+    return SCInput(
+      key: key,
+      labelText: labelText,
+      focusNode: focusNode,
+      hintText: hintText,
+      isPassword: true,
+      suffixIcon: suffixIcon,
+      obscureText: true,
+    );
+  }
+
+  /// Factory constructor for regular text input
+  factory SCInput.textField({
+    required String labelText,
+    Key? key,
+    FocusNode? focusNode,
+    String? hintText,
+  }) {
+    return SCInput(
+      key: key,
+      labelText: labelText,
+      focusNode: focusNode,
+      hintText: hintText,
+    );
+  }
+
+  /// Factory constructor for email input
+  factory SCInput.email({
+    required String labelText,
+    Key? key,
+    FocusNode? focusNode,
+    String? hintText,
+  }) {
+    return SCInput(
+      key: key,
+      labelText: labelText,
+      focusNode: focusNode,
+      hintText: hintText,
+      keyboardType:
+          // Set the keyboard type to email
+          TextInputType.emailAddress,
+    );
+  }
+
   /// Properties
-  /// Controller for text input
-  final TextEditingController controller;
-
-  /// Label text for the input field
+  final String? errorMessage;
+  final bool isPassword;
+  final String? hintText;
   final String labelText;
-
-  /// Hint text for the input field
-  final String hintText;
-
-  /// Validation function for the input
-  final FormFieldValidator<String> validator;
-
-  /// Keyboard type for input (default: text)
-  final TextInputType keyboardType;
-
-  /// Whether input is obscured (default: not obscured)
+  final FocusNode? focusNode;
+  final Widget? suffixIcon;
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? validator;
+  final TextInputType? keyboardType;
   final bool obscureText;
 
   @override
   Widget build(BuildContext context) {
-    /// This widget builds a TextFormField with provided properties
+    /// Build the TextFormField with provided properties
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
@@ -44,76 +91,6 @@ class SCTextFormField extends StatelessWidget {
       validator: validator,
       keyboardType: keyboardType,
       obscureText: obscureText,
-    );
-  }
-}
-
-/// Subclass of SCTextFormField for handling usernames
-class SCUsernameField extends SCTextFormField {
-  const SCUsernameField({
-    // Inherits controller from parent
-    required super.controller,
-    // Inherits validator from parent
-    required super.validator,
-    super.key,
-  }) : super(
-          // Default label text
-          labelText: 'Username',
-          // Default hint text
-          hintText: 'Enter your username',
-        );
-
-  /// Factory method to create an instance with default properties
-  factory SCUsernameField.create({
-    TextEditingController? controller,
-    FormFieldValidator<String>? validator,
-  }) {
-    return SCUsernameField(
-      controller: controller ?? TextEditingController(),
-      validator: validator ??
-          (value) {
-            if (value == null || value.isEmpty) {
-              return 'Please enter a username';
-            }
-            return null;
-          },
-    );
-  }
-}
-
-/// Subclass of SCTextFormField for handling passwords
-class SCPasswordField extends SCTextFormField {
-  const SCPasswordField({
-    // Inherits controller from parent
-    required super.controller,
-    // Inherits validator from parent
-    required super.validator,
-    super.key,
-  }) : super(
-          // Default label text
-          labelText: 'Password',
-          // Default hint text
-          hintText: 'Enter your password',
-          // Password input is obscured
-          obscureText: true,
-        );
-  // Factory method to create an instance with default properties
-  factory SCPasswordField.create({
-    TextEditingController? controller,
-    FormFieldValidator<String>? validator,
-  }) {
-    return SCPasswordField(
-      controller: controller ?? TextEditingController(),
-      validator: validator ??
-          (value) {
-            if (value == null || value.isEmpty) {
-              return 'Please enter a password';
-            }
-            if (value.length < 8) {
-              return 'Password must be at least 8 characters long';
-            }
-            return null;
-          },
     );
   }
 }
