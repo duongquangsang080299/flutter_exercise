f70c3c8c47f40492ac880eb963e3e05abbb54151
duongquangsang080299 <duongquangsang080299@gmail.com>
2023-09-06 14:52:55 +0700

    Update App bar

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:soccer_club_app/widgets/text.dart';
 
 /// Define AppBar
@@ -7,71 +6,54 @@ class SCAppBar extends StatelessWidget {
   /// Constructor for the SCAppBar widget
   const SCAppBar({
     required this.title,
-    this.subtitle,
-    this.onPressed,
-    this.leading,
-    this.actions,
     super.key,
+    this.subtitle,
+    this.leading,
+    this.actions= const [],
     this.backgroundColor,
     this.centerTitle,
   });
 
   /// Factory constructor for creating a home app bar
-  factory SCAppBar.home({
+  factory SCAppBar.main({
     required String title,
     Color? backgroundColor,
     bool? centerTitle,
-    List<Widget>? actions,
     Widget? leading,
   }) {
     return SCAppBar(
       backgroundColor: backgroundColor,
       title: title,
       leading: leading,
-      actions: actions,
       centerTitle: centerTitle,
     );
   }
 
   /// Factory constructor for creating a non-home app bar
-  factory SCAppBar.secondHome({
+  factory SCAppBar.second({
     required String title,
     Color? backgroundColor,
-    List<Widget>? actions,
     bool? centerTitle,
   }) {
     return SCAppBar(
       title: title,
       backgroundColor: backgroundColor,
-      actions: actions,
       centerTitle: centerTitle,
     );
   }
 
   final String title;
   final String? subtitle;
-  final VoidCallback? onPressed;
   final Widget? leading;
-  final List<Widget>? actions;
+  final List<Widget> actions;
   final Color? backgroundColor;
   final bool? centerTitle;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      /// Adjust title spacing based on presence of leading widget
-      titleSpacing: leading == null ? 0 : NavigationToolbar.kMiddleSpacing,
-
-      /// Set leading width based on presence of leading widget
-      leadingWidth: leading == null ? 0 : 56,
-
       /// Define leading widget
-      leading: leading != null
-          ? IconButton(
-              icon: leading ?? const SizedBox.shrink(),
-              onPressed: () => onPressed ?? context.pop(),
-            )
-          : const SizedBox.shrink(),
+      leading: leading ?? const SizedBox.shrink(),
       backgroundColor: backgroundColor,
       title: Column(
         children: [
@@ -79,9 +61,7 @@ class SCAppBar extends StatelessWidget {
           if (subtitle?.isNotEmpty ?? false) Text(subtitle ?? ''),
         ],
       ),
-      actions: [
-        ...actions ?? [],
-      ],
+      actions: actions,
       centerTitle: centerTitle ?? true,
     );
   }
