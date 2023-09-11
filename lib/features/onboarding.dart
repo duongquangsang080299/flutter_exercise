f80193bb77d3bf9ca7e5e13dfb47466926afdd71
duongquangsang080299 <duongquangsang080299@gmail.com>
2023-09-11 09:07:26 +0700

    Fix: Update UI OnBoarding Screen

--- lib/features/onboarding.dart
+++ lib/features/onboarding.dart
@@ -5,10 +5,10 @@ import 'package:soccer_club_app/core/extention.dart';
 import 'package:soccer_club_app/core/typography/app_fontweight.dart';
 import 'package:soccer_club_app/data/models/user_model.dart';
 import 'package:soccer_club_app/l10n/l10n.dart';
-import 'package:soccer_club_app/presentation/auth/onboarding_content.dart';
 import 'package:soccer_club_app/utils/size_utils.dart';
 import 'package:soccer_club_app/widgets/button.dart';
 import 'package:soccer_club_app/widgets/dots_indicator.dart';
+import 'package:soccer_club_app/widgets/text.dart';
 
 /// Define the OnBoardingPage widget
 class OnBoardingPage extends StatefulWidget {
@@ -49,7 +49,7 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
                       _pageIndex = index;
                     });
                   },
-                  itemBuilder: (context, index) => OnBoardingPageContent(
+                  itemBuilder: (context, index) => OnBoardingBody(
                     image: demoData[index].image,
                     title: demoData[index].title,
                     description: demoData[index].description,
@@ -65,7 +65,7 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
                       children: List.generate(
                         demoData.length,
                         (index) => Padding(
-                          padding: EdgeInsets.all(context.getHorizontalSize(8)),
+                          padding: const EdgeInsets.all(8),
                           child: SCDotsIndicator(
                             isActive: index == _pageIndex,
                           ),
@@ -77,34 +77,35 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        SCOutlineButton(
-                          onPressed: () {
-                            context.go('/welcomeScreen');
-                          },
-                          // Localize the 'Skip' text using context.l10n
-                          text: context.l10n.skip,
-                          style: context.textTheme.displayMedium?.copyWith(
-                            color: AppColor.jetBlackColor,
-                            fontWeight: AppFontWeight.semiBold,
+                        SizedBox(
+                          width: context.getHorizontalSize(154),
+                          child: SCOutlineButton(
+                            onPressed: () {
+                              context.go('/welcomeScreen');
+                            },
+                            // Localize the 'Skip' text using context.l10n
+                            text: context.l10n.skip,
+                            style: context.textTheme.displayMedium?.copyWith(
+                              color: AppColor.jetBlackColor,
+                              fontWeight: AppFontWeight.semiBold,
+                            ),
                           ),
-                          // Use getHorizontalSize and getVerticalSize
-                          width: context.getHorizontalSize(164),
-                          height: context.getVerticalSize(60),
                         ),
                         const SizedBox(width: 20),
-                        SCButton(
-                          onPressed: () {
-                           context.go('/welcomeScreen');
-                          },
-                          // Localize the 'CAMPAIGNS' text using context.l10n
-                          text: context.l10n.campaigns,
-                          style: context.textTheme.displayMedium?.copyWith(
-                            color: AppColor.tertiary,
-                            fontWeight: AppFontWeight.semiBold,
+                        SizedBox(
+                          width: context.getHorizontalSize(154),
+                          child: SCButton(
+                            onPressed: () {
+                              context.go('/welcomeScreen');
+                            },
+                            // Localize the 'CAMPAIGNS' text using context.l10n
+                            text: context.l10n.campaigns,
+                            style: context.textTheme.displayMedium?.copyWith(
+                              color: AppColor.tertiary,
+                              fontWeight: AppFontWeight.semiBold,
+                            ),
+                            backgroundColor: AppColor.primaryColor,
                           ),
-                          backgroundColor: AppColor.primaryColor,
-                          width: context.getHorizontalSize(164),
-                          height: context.getVerticalSize(60),
                         ),
                       ],
                     ),
@@ -119,9 +120,8 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
   }
 }
 
-/// Create the OnBoardingPageContent widget
-class OnBoardingPageContent extends StatelessWidget {
-  const OnBoardingPageContent({
+class OnBoardingBody extends StatelessWidget {
+  const OnBoardingBody({
     required this.image,
     required this.title,
     required this.description,
@@ -141,8 +141,8 @@ class OnBoardingPageContent extends StatelessWidget {
         SizedBox(height: context.getVerticalSize(90)),
         Image.asset(
           image,
-          width: context.getHorizontalSize(243),
-          height: context.getVerticalSize(309),
+          width: context.getSize(243),
+          height: context.getSize(309),
         ),
         // Add vertical spacing using the getVerticalSize method
         SizedBox(height: context.getVerticalSize(60)),
@@ -164,7 +164,7 @@ class OnBoardingPageContent extends StatelessWidget {
         const SizedBox(height: 20),
         SCText.displaySmall(
           context,
-          style: context.textTheme.bodyLarge?.copyWith(
+          style: context.textTheme.displaySmall?.copyWith(
             color: AppColor.tertiary,
             fontWeight: AppFontWeight.regular,
           ),
