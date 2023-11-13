0d4786d8f29cb52ef602f6991baccef145ce420a
duongquangsang080299 <duongquangsang080299@gmail.com>
2023-11-13 11:31:01 +0700

    Fix: Hanlde Error Exception

---
+++ lib/core/constant/app_exceptions.dart
@@ -0,0 +1,20 @@
+class AppExceptionMessages {
+  static const String connectionTimeout = 'Connection timed out';
+  static const String sendTimeout = 'Request timed out';
+  static const String receiveTimeout = 'Response timeout';
+  static const String unknown = 'Unknown error';
+  static const String connectionError = 'No internet connection';
+  static const String cancel = 'Request cancelled';
+  static const String badCertificate = 'Bad certificate';
+  static const String badRequest = 'Bad Request';
+  static const String unauthorized = 'Unauthorized';
+  static const String forbidden = 'Forbidden';
+  static const String notFound = 'Not Found';
+  static const String methodNotAllowed = 'Method not allowed';
+  static const String internalServerError = 'Internal Server Error';
+  static const String badGateway = 'Bad gateway';
+  static const String serviceUnavailable = 'Service unavailable';
+  static const String httpNotSupport = 'Does not support HTTP protocol request';
+  static const String unknownMistake = 'Unknown mistake';
+  static const String unknownError = 'Unknown error';
+}
