e34c16caf5257a5bccde91c51032393f17c4a9e0
duongquangsang080299 <duongquangsang080299@gmail.com>
2023-11-21 15:35:10 +0700

    Setup bloc for home page

--- lib/blocs/home/home_event.dart
+++ lib/blocs/home/home_event.dart
@@ -1,8 +1,18 @@
 part of 'home_bloc.dart';
 
-sealed class HomeEvent extends Equatable {
+abstract class HomeEvent extends Equatable {
   const HomeEvent();
 
   @override
   List<Object> get props => [];
 }
+
+class FetchDataEvent extends HomeEvent {}
+
+class NextMatchEvent extends HomeEvent {}
+
+class NewsEvent extends HomeEvent {}
+
+class LatestResultsEvent extends HomeEvent {}
+
+class LiveMatchEvent extends HomeEvent {}
