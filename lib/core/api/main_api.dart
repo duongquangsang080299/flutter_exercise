87f18478a700c1125dbe05665dcda045fd6f2d16
duongquangsang080299 <duongquangsang080299@gmail.com>
2023-11-24 08:18:28 +0700

    Refactor code home repo and api consumer

--- lib/core/api/main_api.dart
+++ lib/core/api/main_api.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:soccer_club_app/core/error/error_exception.dart';
 
 class ApiConsumer {
+  // Private method to handle HTTP response
   static Future<dynamic> _handleResponse(http.Response response) {
     if (response.statusCode >= 200 && response.statusCode < 300) {
       return json.decode(response.body);
@@ -12,43 +13,56 @@ class ApiConsumer {
     }
   }
 
-  static Future<dynamic> get(String url) async {
+  // Generic HTTP GET method
+  static Future<dynamic> get(String url, {String? apiKey}) async {
     try {
-      final response = await http.get(Uri.parse(url));
+      // Include the API key in the headers if provided
+      final headers = apiKey != null ? {'apiKey': apiKey} : null;
+
+      final response = await http.get(Uri.parse(url), headers: headers);
       return _handleResponse(response);
     } catch (e) {
       throw AppException(AppExceptionType.badResponse);
     }
   }
 
+  // Generic HTTP POST method
   static Future<dynamic> post(String url, Map<String, dynamic> data) async {
     try {
-      final response = await http.post(Uri.parse(url), body: json.encode(data));
+      final response = await http.post(Uri.parse(url),
+          body: json.encode(data),
+          headers: {'Content-Type': 'application/json'});
       return _handleResponse(response);
     } catch (e) {
       throw AppException(AppExceptionType.badResponse);
     }
   }
 
+  // Generic HTTP PUT method
   static Future<dynamic> put(String url, Map<String, dynamic> data) async {
     try {
-      final response = await http.put(Uri.parse(url), body: json.encode(data));
+      final response = await http.put(Uri.parse(url),
+          body: json.encode(data),
+          headers: {'Content-Type': 'application/json'});
       return _handleResponse(response);
     } catch (e) {
       throw AppException(AppExceptionType.badResponse);
     }
   }
 
+  // Generic HTTP PATCH method
   static Future<dynamic> patch(String url, Map<String, dynamic> data) async {
     try {
-      final response =
-          await http.patch(Uri.parse(url), body: json.encode(data));
+      final response = await http.patch(Uri.parse(url),
+          body: json.encode(data),
+          headers: {'Content-Type': 'application/json'});
       return _handleResponse(response);
     } catch (e) {
       throw AppException(AppExceptionType.badResponse);
     }
   }
 
+  // Generic HTTP DELETE method
   static Future<dynamic> delete(String url) async {
     try {
       final response = await http.delete(Uri.parse(url));
--- lib/data/repositories/home_repo.dart
+++ lib/data/repositories/home_repo.dart
@@ -1,17 +1,17 @@
-// home_repository.dart
 import 'package:soccer_club_app/core/api/main_api.dart';
 import 'package:soccer_club_app/core/constant/api_key.dart';
 import 'package:soccer_club_app/presentations/view_models/home/home_view_models.dart';
 
 class HomeRepository {
   final ApiConsumer apiConsumer = ApiConsumer();
-
   Future<HomeViewModel> fetchHomeData({String apiKey = ApiKey.apiKey}) async {
     try {
-      final dynamic responseData = await ApiConsumer.get(
-        'https://olttmjkegsnbhrupogiu.supabase.co/rest/v1/HomeViewModel',
-        apiKey: apiKey,
-      );
+      const String apiUrl =
+          'https://olttmjkegsnbhrupogiu.supabase.co/rest/v1/home';
+
+      final dynamic responseData =
+          await ApiConsumer.get(apiUrl, apiKey: apiKey);
+
       final HomeViewModel homeViewModel = HomeViewModel.fromJson(responseData);
       return homeViewModel;
     } catch (e) {
