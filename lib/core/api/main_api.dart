bf9538204173f7a16cb042180f42fb47df882b2a
duongquangsang080299 <duongquangsang080299@gmail.com>
2023-11-27 16:53:35 +0700

    fix comment for handle api

--- lib/core/api/main_api.dart
+++ lib/core/api/main_api.dart
@@ -1,26 +1,30 @@
 import 'package:dio/dio.dart';
+import 'package:soccer_club_app/core/constant/apis.dart';
+import 'package:soccer_club_app/core/error/error_exception.dart';
 
-class ApiConsumer {
+class MainApi {
   final Dio _dio = Dio();
 
-  Future<dynamic> get(String url, {String? apiKey}) async {
+  Future<dynamic> get(String url,
+      {String? apiKey, Map<String, dynamic>? queryParams}) async {
     try {
       final response = await _dio.get(
         url,
         options: Options(
-          headers: {'apikey': apiKey},
+          headers: {'apikey': apiKey = Apis.apiKey},
         ),
+        queryParameters: queryParams,
       );
 
       // Check if the response is successful
       if (response.statusCode == 200) {
         return response;
       } else {
-        throw Exception(
-            'Failed to load data. Status code: ${response.statusCode}');
+        throw ErrorException(
+            message: 'Failed to load data. Status code ${response.statusCode}');
       }
     } catch (e) {
-      throw Exception('Failed to make the request: $e');
+      throw ErrorException(message: 'Failed to make the request: $e');
     }
   }
 }
